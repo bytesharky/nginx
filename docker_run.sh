ce1f0d10ef45358cef88854b5f128d4355b8887f
bytesharky <fish@doffish.com>
2025-08-30 20:59:16 +0800

    docker部署脚本

--- docker_run.sh
+++ docker_run.sh
@@ -2,14 +2,26 @@
 set -e
 
 ROOT_PATH="/data/docker/nginx"
+IMAGE_NAME="sharky-nginx"
+CONTAINER_NAME="sharky-nginx"
+TAG="latest"
+
+if [ ! -d "$ROOT_PATH" ]; then
+  mkdir -p "$ROOT_PATH/conf"
+  mkdir -p "$ROOT_PATH/logs"
+fi
+
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+  echo "$MSG_EXIST $CONTAINER_NAME"
+  docker rm -f "$CONTAINER_NAME"
+fi
 
 docker run -d \
   -p 80:80 \
   -p 443:443/tcp \
   -p 443:443/udp \
   -e TZ=Asia/Shanghai \
-  --name sharky-nginx \
+  --name "$CONTAINER_NAME" \
   -v $ROOT_PATH/conf:/etc/nginx \
   -v $ROOT_PATH/logs:/var/log/nginx \
-  sharky-nginx:latest
-  
+  "$IMAGE_NAME:$TAG"
